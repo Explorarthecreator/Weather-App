860ed0be12ba9ed838db692019d947e1eeb173d2
Explorarthecreator <fredadejiemma99@gmail.com>
2023-10-08 10:58:30 +0100

    Added all the images and done with the funcionalities

---
+++ javascript/forcast.js
@@ -0,0 +1,37 @@
+const key = 'Sq75Zv4UTIWzZHrQH3fZoF6kDqWV8foO';
+
+
+// get weather information
+
+const getWeather = async (id)=>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+
+    // console.log(data)
+}
+
+// get city information
+const getCity = async (city)=>{
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query);
+
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('benin city').then(data=>{
+//     return getWeather(data.Key);
+// }).then(data=>{
+//     console.log(data)
+// })
+// .catch(err=>{
+//     console.log(err)
+// });
---
+++ javascript/index.js
@@ -0,0 +1,88 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const cardDetails = document.querySelector('.details');
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
+
+
+const updateUI = (data)=>{
+
+    // const cityDetails = data.cityDetails;
+    // const weather = data.weather;
+
+    // destructirng data and is exactly like the code above and it makes the code cleaner
+    const{cityDetails,weather}= data;
+
+
+    cardDetails.innerHTML = `
+                <h5>
+                    ${cityDetails.EnglishName}, ${cityDetails.Country.EnglishName}
+                </h5>
+                <div>
+                    ${weather.WeatherText}
+                </div>
+                <div>
+                    <span>
+                        ${weather.Temperature.Metric.Value}
+                    </span>
+                    <span>
+                        &deg;C
+                    </span>
+                </div>
+    `;
+// Updtae the night and day image
+    let timeSrc = weather.IsDayTime? 'img/day.svg':'img/night.svg';
+
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg'
+    // }else{
+    //     timeSrc = 'img/night.svg'
+    // }
+// ternary ooperator 
+
+
+    time.setAttribute('src',timeSrc)
+
+    // update the icon
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+
+    icon.setAttribute('src',iconSrc)
+
+    // remove d-none
+    if(card.classList.contains('none')){
+        card.classList.remove('none')
+    }
+}
+
+const updateCity = async (city)=>{
+
+    // console.log(city)
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    // return{
+    //     cityDetails : cityDetails,
+    //     weather : weather
+    // };
+
+    // object shorthand notation for the above
+    return {
+        cityDetails,weather
+    }
+}
+
+cityForm.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    // get value from form
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // Update the UI
+    updateCity(city).then(data=>{
+        updateUI(data)
+        console.log(data)
+    }).catch(err =>{
+        console.log(err)
+    });
+})
